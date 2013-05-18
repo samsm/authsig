5e6b0428744aca62611de79f32a5e432fdc3c74a
Sam Schenkman-Moore <samsm@samsm.com>
2013-05-18 18:33:04 -0400

    Base app

---
+++ Gemfile
@@ -0,0 +1,41 @@
+source 'https://rubygems.org'
+
+# Distribute your app as a gem
+# gemspec
+
+# Server requirements
+gem 'thin' # or mongrel
+# gem 'trinidad', :platform => 'jruby'
+
+# Optional JSON codec (faster performance)
+# gem 'oj'
+
+# Project requirements
+gem 'rake'
+
+# Component requirements
+gem 'bcrypt-ruby', :require => 'bcrypt'
+gem 'haml'
+gem 'dm-sqlite-adapter'
+gem 'dm-validations'
+gem 'dm-timestamps'
+gem 'dm-migrations'
+gem 'dm-constraints'
+gem 'dm-aggregates'
+gem 'dm-types'
+gem 'dm-core'
+
+gem 'tilt', '1.3.7'
+
+# Test requirements
+
+# Padrino Stable Gem
+gem 'padrino', '0.11.1'
+
+# Or Padrino Edge
+# gem 'padrino', :github => 'padrino/padrino-framework'
+
+# Or Individual Gems
+# %w(core gen helpers cache mailer admin).each do |g|
+#   gem 'padrino-' + g, '0.11.1'
+# end
---
+++ config/apps.rb
@@ -0,0 +1,38 @@
+##
+# This file mounts each app in the Padrino project to a specified sub-uri.
+# You can mount additional applications using any of these commands below:
+#
+#   Padrino.mount('blog').to('/blog')
+#   Padrino.mount('blog', :app_class => 'BlogApp').to('/blog')
+#   Padrino.mount('blog', :app_file =>  'path/to/blog/app.rb').to('/blog')
+#
+# You can also map apps to a specified host:
+#
+#   Padrino.mount('Admin').host('admin.example.org')
+#   Padrino.mount('WebSite').host(/.*\.?example.org/)
+#   Padrino.mount('Foo').to('/foo').host('bar.example.org')
+#
+# Note 1: Mounted apps (by default) should be placed into the project root at '/app_name'.
+# Note 2: If you use the host matching remember to respect the order of the rules.
+#
+# By default, this file mounts the primary app which was generated with this project.
+# However, the mounted app can be modified as needed:
+#
+#   Padrino.mount('AppName', :app_file => 'path/to/file', :app_class => 'BlogApp').to('/')
+#
+
+##
+# Setup global project settings for your apps. These settings are inherited by every subapp. You can
+# override these settings in the subapps as needed.
+#
+Padrino.configure_apps do
+  # enable :sessions
+  set :session_secret, '1e650f2a990677fc61116b202d1e06463439027b1d4f7fc7658dbaa6d747ccc5'
+  set :protection, true
+  set :protect_from_csrf, true
+end
+
+# Mounts the core application for this project
+Padrino.mount('Authsig::App', :app_file => Padrino.root('app/app.rb')).to('/')
+
+Padrino.mount("Authsig::Admin", :app_file => File.expand_path('../../admin/app.rb', __FILE__)).to("/admin")
